f6bf3eb75a4d22a9077d94d7d3099b1f2f1e0b65
1995101 <1995101@LI-S068-0T26>
2019-11-13 13:14:46 -0500

    appCtr modification

--- src/main/java/com/team3/main/AppCtr.java
+++ src/main/java/com/team3/main/AppCtr.java
@@ -1,13 +1,16 @@
 package com.team3.main;
 
+import com.team3.util.FileReader;
+import com.team3.util.FileWriter;
 import com.team3.util.LoggerLocal;
 import com.team3.util.UtilJson;
+import java.io.IOException;
 
 public class AppCtr {
     
-    public static void main(String[] args) {
-        String fichierEntree = "ressources/terrain1.json";
-        String fichierSortie = "ressources/resultat.json";
+    public static void main(String[] args) throws IOException { 
+        String fichierEntree = FileReader.loadFileIntoString(args[0], "UTF-8");
+        String fichierSortie = args[1].toString();
         try {
             UtilJson.traiterFichierJson(fichierEntree, fichierSortie);
         } catch (Exception e) {
