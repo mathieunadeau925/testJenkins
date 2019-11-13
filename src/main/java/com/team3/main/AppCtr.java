8974d74ac8540a449a71a47d8b56af44c04d29ea
1995046 <1995046@LI-S068-0T29>
2019-11-13 17:34:58 -0500

    Ajout d'un bloc try catch pour gérer les fichiers mal entrés.

--- src/main/java/com/team3/main/AppCtr.java
+++ src/main/java/com/team3/main/AppCtr.java
@@ -1,20 +1,18 @@
 package com.team3.main;
 
 import com.team3.util.FileReader;
-import com.team3.util.FileWriter;
 import com.team3.util.LoggerLocal;
 import com.team3.util.UtilJson;
-import java.io.IOException;
-
 public class AppCtr {
-    
-    public static void main(String[] args) throws IOException { 
-        String fichierEntree = FileReader.loadFileIntoString(args[0], "UTF-8");
-        String fichierSortie = args[1].toString();
+
+    public static void main(String[] args) {
         try {
+            String fichierEntree = FileReader.loadFileIntoString(args[0], "UTF-8");
+            String fichierSortie = args[1].toString();
             UtilJson.traiterFichierJson(fichierEntree, fichierSortie);
         } catch (Exception e) {
             LoggerLocal.logErreur(e);
+            System.out.println("Erreur dans vos fichiers json!");
         }
     }
 }
