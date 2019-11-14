cccf244079ea098da9b7ab1e4e5b45fdebe4e2c0
marhoum <marhoum@marhoum>
2019-11-14 08:30:10 -0500

    Class UtilCalcul méthode arrondisseurMultiple5Superieur mise en commentaire Ajout de méthode de test sur la class CalculCommercialTest testCalculerValeurLot testCalculerDroitPassage testCalculerMontantServiceINF testCalculerMontantServiceSUP testCalculerMontantServiceMAX

--- src/test/java/com/team3/util/CalculCommercialTest.java
+++ src/test/java/com/team3/util/CalculCommercialTest.java
@@ -1,36 +1,106 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
 package com.team3.util;
 
 import org.junit.After;
 import org.junit.AfterClass;
+import static org.junit.Assert.assertEquals;
 import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
-import static org.junit.Assert.*;
 
+/**
+ *
+ * @author Rabi MARHOUM
+ */
 public class CalculCommercialTest {
-    
+
     public CalculCommercialTest() {
     }
-    
+
     @BeforeClass
     public static void setUpClass() {
     }
-    
+
     @AfterClass
     public static void tearDownClass() {
     }
-    
+
     @Before
     public void setUp() {
     }
-    
+
     @After
     public void tearDown() {
     }
 
-    // TODO add test methods here.
-    // The methods must be annotated with annotation @Test. For example:
-    //
-    // @Test
-    // public void hello() {}
+    /**
+     * Test of calculerValeurLot method, of class CalculCommercial.
+     */
+    @Test
+    public void testCalculerValeurLot() {
+        System.out.println("calculerValeurLot");
+        double superficie = 300.0;
+        double prixMax = 4.0;
+        double expResult = 1200.0;
+        double result = CalculCommercial.calculerValeurLot(superficie, prixMax);
+        assertEquals(expResult, result, 0.0);
+
+    }
+
+    /**
+     * Test of calculerDroitPassage method, of class CalculCommercial.
+     */
+    @Test
+    public void testCalculerDroitPassage() {
+        System.out.println("calculerDroitPassage");
+        double nbrDoitPassage = 4.0;
+        double valeurLot = 933.0;
+        double expResult = -59.8;
+        double result = CalculCommercial.calculerDroitPassage(nbrDoitPassage, valeurLot);
+        assertEquals(expResult, result, 0.0);
+
+    }
+
+    /**
+     * Test of calculerMontantService method, of class CalculCommercial.
+     */
+    @Test
+    public void testCalculerMontantServiceINF() {
+        System.out.println("calculerMontantService");
+        double superficie = 400.0;
+        int nombre_services = 3;
+        double expResult = 1500.0;
+        double result = CalculCommercial.calculerMontantService(superficie, nombre_services);
+        assertEquals(expResult, result, 0.0);
+    }
+
+    /**
+     * Test of calculerMontantService method, of class CalculCommercial.
+     */
+    @Test
+    public void testCalculerMontantServiceSUP() {
+        System.out.println("calculerMontantService");
+        double superficie = 600.0;
+        int nombre_services = 3;
+        double expResult = 4500.0;
+        double result = CalculCommercial.calculerMontantService(superficie, nombre_services);
+        assertEquals(expResult, result, 0.0);
+    }
+
+    /**
+     * Test of calculerMontantService method, of class CalculCommercial.
+     */
+    @Test
+    public void testCalculerMontantServiceMAX() {
+        System.out.println("calculerMontantService");
+        double superficie = 400.0;
+        int nombre_services = 5;
+        double expResult = 2500.0;
+        double result = CalculCommercial.calculerMontantService(superficie, nombre_services);
+        assertEquals(expResult, result, 0.0);
+    }
 }
